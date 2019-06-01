f533fadd9a41840f16138f058961e929115ea63f
Luciano Rodríguez <luciano@siteplan.io>
2019-06-01 12:36:16 -0300

    search services

---
+++ lib/models/repo.dart
@@ -0,0 +1,14 @@
+class Repo {
+  String url;
+  String fullName;
+  String description;
+  num score;
+
+  Repo.fromMap(Map<String, dynamic> map) {
+    this.url = map['url'];
+    this.fullName = map['full_name'];
+    this.description = map['description'];
+    this.score = map['score'];
+  }
+
+}
---
+++ lib/services/githubApiClient.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_hackaton/models/repo.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class GithubApiClient {
+  static final _instance = GithubApiClient._internal();
+  factory GithubApiClient() => _instance;
+  final _decoder = JsonDecoder();
+
+  static const String url = "https://api.github.com/search/repositories?q=flutter";
+  GithubApiClient._internal();
+  
+  Future<List<Repo>> search() async {
+    var repos = List<Repo>();
+    var response = await http.get(url);
+      if (response.statusCode == 200) {
+        final body = _decoder.convert(response.body);
+        body['items'].forEach((item) => repos.add(Repo.fromMap(item as Map)));
+      }
+    return repos;
+  }
+
+}
