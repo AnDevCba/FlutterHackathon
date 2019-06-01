13c93c774e16be659d7367bb79988428b0784bb4
Gaston Saillen <gaston_luque_29@hotmail.com>
2019-06-01 14:39:34 -0300

    open github url

--- lib/repolist/model/repo.dart
+++ lib/repolist/model/repo.dart
@@ -5,7 +5,7 @@ class Repo {
   num score;
 
   Repo.fromMap(Map<String, dynamic> map) {
-    this.url = map['url'];
+    this.url = map['html_url'];
     this.fullName = map['full_name'];
     this.description = map['description'];
     this.score = map['score'];
